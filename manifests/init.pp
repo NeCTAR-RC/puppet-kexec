46b01e509494a4fb23bd011ebf43c35d5bb200ce
Marcus Furlong <furlongm@gmail.com>
2014-05-09 15:56:11 +1000

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,15 @@
+class kexec {
+
+  package { 'kexec-tools':
+    ensure => installed,
+  }
+
+  file { '/etc/default/kexec':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('kexec/etc-default-kexec.erb'),
+    require => package['kexec-tools'],
+  }
+}
