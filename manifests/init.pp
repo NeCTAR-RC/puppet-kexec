f6991a2aa5008902cb1ab0aac3455e17c6e7d63a
Justin Mammarella <jmamma@gmail.com>
2014-11-14 13:48:40 +1100

    Kexec Module - Bug fix for Trusty: https://bugs.launchpad.net/ubuntu/+source/kexec-tools/+bug/1383096
    Change-Id: I57e636cff02a2d126f74829356a5dd054529343c

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,22 @@ class kexec {
     ensure => installed,
   }
 
+  #Bug Fix: https://bugs.launchpad.net/ubuntu/+source/kexec-tools/+bug/1383096
+
+  case $::lsbdistcodename {
+    default: {}
+    trusty: {
+      file { '/etc/init.d/K18kexec-load':
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => '0777',
+        require => package['kexec-tools'],
+        source  => 'puppet:///modules/kexec/etc-init.d-K18kexec-load',
+      }
+    }
+  }
+
   file { '/etc/default/kexec':
     ensure  => present,
     owner   => root,
@@ -12,4 +28,5 @@ class kexec {
     content => template('kexec/etc-default-kexec.erb'),
     require => package['kexec-tools'],
   }
+
 }
