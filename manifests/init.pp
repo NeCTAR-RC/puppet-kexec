cd8af857c41edc2c2d5355842455f815432357e7
Justin Mammarella <jmamma@gmail.com>
2014-11-14 13:48:40 +1100

    Kexec Module: Bug fix for Trusty. If lsbdist == Trusty, Init.pp will replace /etc/init.d/K18kexec-load with a modified version located in modules/kexec/files
    Change-Id: I57e636cff02a2d126f74829356a5dd054529343c

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,12 +4,26 @@ class kexec {
     ensure => installed,
   }
 
+  case $::lsbdistcodename {
+    trusty: {
+      file { '/etc/init.d/K18kexec-load':
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => '0777',
+        require => package['kexec-tools'],
+        source  => 'puppet:///modules/kexec/etc-init.d-K18kexec-load',
+      }
+    }
+  }
+
   file { '/etc/default/kexec':
     ensure  => present,
     owner   => root,
     group   => root,
     mode    => '0644',
     content => template('kexec/etc-default-kexec.erb'),
-    require => package['kexec-tools'],
+    require => Package['kexec-tools'],
   }
+
 }
